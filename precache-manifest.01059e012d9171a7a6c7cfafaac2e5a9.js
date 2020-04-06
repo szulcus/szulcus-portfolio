017f45c3d9d53484a9338708960d944cacda5d4b
jakub104 <jakubzlezena@wp.pl>
2020-04-06 22:23:40 +0200

    Updates

---
+++ precache-manifest.01059e012d9171a7a6c7cfafaac2e5a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48e94e328e45f9f7b0e739f1765c322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbc0d7ac5dea09b8b9c",
+    "url": "/static/css/main.6501c31d.chunk.css"
+  },
+  {
+    "revision": "f4fa18e6ee364e206ea9",
+    "url": "/static/js/2.3d6e4b65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d6e4b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdbc0d7ac5dea09b8b9c",
+    "url": "/static/js/main.800c99f7.chunk.js"
+  },
+  {
+    "revision": "ae5e19a8f56d350e0839",
+    "url": "/static/js/runtime-main.9b047ee8.js"
+  },
+  {
+    "revision": "b7ad6d082febe0e215f76c3b7fd6a5dc",
+    "url": "/static/media/avatar.b7ad6d08.jpg"
+  },
+  {
+    "revision": "19552b4f783c3a984f112b58cef4df82",
+    "url": "/static/media/easycalc.19552b4f.png"
+  },
+  {
+    "revision": "08adfec67aab6e7764b839ce99ac6ab0",
+    "url": "/static/media/easymath.08adfec6.png"
+  },
+  {
+    "revision": "cf05a6523d09059a358283eb074ab125",
+    "url": "/static/media/easyword.cf05a652.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/szulcus-portfolio/precache-manifest.b1da1ad671f21b64dbef266c27683e13.js"
+  "/precache-manifest.01059e012d9171a7a6c7cfafaac2e5a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/szulcus-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
